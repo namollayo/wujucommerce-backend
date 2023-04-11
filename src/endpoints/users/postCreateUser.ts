5e6c7760dfe762e76e9cf2280a39da5e8daacc7d
namollayo <gabisantcarrara@gmail.com>
2023-04-10 22:08:11 -0300

    update the endpoints methods

--- src/endpoints/users/postCreateUser.ts
+++ src/endpoints/users/postCreateUser.ts
@@ -3,14 +3,32 @@ import { users } from "../../database"
 import { TUser } from "../../types"
 
 export const createUser = (req: Request, res: Response) => {
-    const { id, name, email, password } = req.body
-    const newUser : TUser = {
-        id,
-        name,
-        email,
-        password
-    }
+    try {
+        const { id, name, email, password } = req.body
 
-    users.push(newUser)
-    return res.status(201).send('Registration Successfully Done')
+        const newUser : TUser = {
+            id,
+            name,
+            email,
+            password
+        }
+    
+        // if (!id || !name || !email || !password) {
+        //     if()
+        //     res.status(400)
+        //     throw new Error("");
+        // }
+
+        users.push(newUser)
+        return res.status(201).send('Registration Successfully Done')
+    } catch (error) {        
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+        if ( error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Unexpected error")
+        }
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ import { createPurchase } from './endpoints/purchases/postCreatePurchases'
 import { createUser } from './endpoints/users/postCreateUser'
 import { removeProduct } from './endpoints/products/delProductById'
 import { removeUser } from './endpoints/users/delUserById'
+import { editUser } from './endpoints/users/putUserById'
+import { editProduct } from'./endpoints/products/putProductById'
+
 
 const app = express()
 
@@ -23,13 +26,14 @@ app.listen(3003, () => {
 app.get('/users', getAllUsers)
 app.post('/users', createUser)
 app.delete('/users/:id', removeUser)
-app.put('/users/:id', removeUser)
+app.put('/users/:id', editUser)
 
 app.get('/products', getAllProducts)
 app.get('/products/search', getProductByName)
 app.get('/products/:id', getProductsById)
 app.post('/products', createProduct)
 app.delete('/products/:id', removeProduct)
+app.put('/products/:id', editProduct)
 
 app.post('/purchases', createPurchase)
 app.get('/users/:id/purchases', getUserPurchasesByUserId)
